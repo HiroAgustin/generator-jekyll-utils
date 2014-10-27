6c808efe0a62c057ad2d389bb69787f5122e023a
HiroAgustin <hiroagustin@gmail.com>
2014-10-27 17:24:50 -0400

    add sub generator

---
+++ test/test-post.js
@@ -0,0 +1,22 @@
+/*global describe, beforeEach, it*/
+'use strict';
+
+var path = require('path');
+var assert = require('yeoman-generator').assert;
+var helpers = require('yeoman-generator').test;
+var os = require('os');
+
+describe('jekyll-utils:post', function () {
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../post'))
+      .inDir(path.join(os.tmpdir(), './temp-test'))
+      .withArguments('name', '--force')
+      .on('end', done);
+  });
+
+  it('creates files', function () {
+    assert.file([
+      'somefile.js'
+    ]);
+  });
+});
