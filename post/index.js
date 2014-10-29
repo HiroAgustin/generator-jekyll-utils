bf1a210c6a9e94e1d8461b0d0ac1231ea68f1f53
HiroAgustin <hiroagustin@gmail.com>
2014-10-29 16:38:20 -0400

    initial version is done

--- post/index.js
+++ post/index.js
@@ -1,4 +1,4 @@
-;(function (yosay, yeoman)
+;(function (yosay, yeoman, fs, path)
 {
   'use strict';
 
@@ -6,32 +6,81 @@
 
     initializing: function ()
     {
+      this.root = this.getRootPath();
+
+      this.destinationRoot(path.join(this.root, '_posts'));
+
       this.name = Array.prototype.join.call(arguments, ' ');
+    }
+
+  , getRootPath: function ()
+    {
+      var cwd = process.cwd()
+        , oneUp = path.join(cwd, '..')
+        , onApp = path.join(cwd, 'app');
+
+      if (fs.existsSync(path.join(cwd, '_layouts')))
+        return cwd;
+
+      if (fs.existsSync(path.join(oneUp, '_layouts')))
+        return oneUp;
+
+      if (fs.existsSync(path.join(onApp, '_layouts')))
+        return onApp;
     }
 
   , prompting: function ()
     {
-      var done = this.async()
-        , name = this.name;
+      var name = this.name
+        , root = this.root
+        , done = this.async();
 
       // Have Yeoman greet the user.
-      this.log(yosay('Writing a post?\nI can help with that!'));
+      this.log(yosay('Writing a post? I can help with that!'));
 
       this.prompt(
         [
           {
             type: 'input'
           , name: 'name'
-          , message: 'What would be the title?'
+          , message: 'What is the post title?'
           , when: function ()
             {
               return !name;
             }
           }
         , {
-            type: 'input'
+            type: 'confirm'
+          , name: 'slug'
+          , message: 'Use title as slug?'
+          , default: true
+          }
+        , {
+            type: 'list'
           , name: 'layout'
-          , message: 'What would be the layout?'
+          , message: 'Which layout would you like to use?'
+          , default: 'post'
+          , choices: function ()
+            {
+              return fs.readdirSync(path.join(root, '_layouts')).map(function (name)
+              {
+                return name.split('.')[0];
+              });
+            }
+          }
+        , {
+            type: 'list'
+          , name: 'extention'
+          , message: 'Which file extention?'
+          , default: 'md'
+          , choices: [
+              'md'
+            , 'mkd'
+            , 'mkdn'
+            , 'mkdown'
+            , 'textile'
+            , 'markdown'
+            ]
           }
         ]
 
@@ -42,9 +91,9 @@
           this.date = isoDate[0];
           this.time = isoDate[1].split('.')[0];
 
+          this.extention = props.extention;
           this.name = name || props.name;
           this.layout = props.layout;
-          this.extention = 'md';
 
           done();
 
@@ -58,4 +107,4 @@
     }
   });
 
-}(require('yosay'), require('yeoman-generator')));
+}(require('yosay'), require('yeoman-generator'), require('fs'), require('path')));
