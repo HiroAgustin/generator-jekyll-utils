1a8e819a42a49974a98c084b2771f95cbea58b87
HiroAgustin <hiroagustin@gmail.com>
2014-10-29 13:39:47 -0400

    create post using date as name, options title and layout

--- post/index.js
+++ post/index.js
@@ -1,20 +1,61 @@
-;(function (util, yeoman)
+;(function (yosay, yeoman)
 {
   'use strict';
 
-  var JekyllUtilsGenerator = yeoman.generators.NamedBase.extend({
+  module.exports = yeoman.generators.Base.extend({
 
     initializing: function ()
     {
-      this.log('You called the jekyll-utils subgenerator with the argument ' + this.name + '.');
+      this.name = Array.prototype.join.call(arguments, ' ');
+    }
+
+  , prompting: function ()
+    {
+      var done = this.async()
+        , name = this.name;
+
+      // Have Yeoman greet the user.
+      this.log(yosay('Writing a post?\nI can help with that!'));
+
+      this.prompt(
+        [
+          {
+            type: 'input'
+          , name: 'name'
+          , message: 'What would be the title?'
+          , when: function ()
+            {
+              return !name;
+            }
+          }
+        , {
+            type: 'input'
+          , name: 'layout'
+          , message: 'What would be the layout?'
+          }
+        ]
+
+      , function (props)
+        {
+          var isoDate = new Date().toISOString().split('T');
+
+          this.date = isoDate[0];
+          this.time = isoDate[1].split('.')[0];
+
+          this.name = name || props.name;
+          this.layout = props.layout;
+          this.extention = 'md';
+
+          done();
+
+        }.bind(this)
+      );
     }
 
   , writing: function ()
     {
-      this.src.copy('somefile.js', 'somefile.js');
+      this.template('post.md', this.date + '-' + this._.slugify(this.name) + '.' + this.extention);
     }
   });
 
-  module.exports = JekyllUtilsGenerator;
-
-}(require('util'), require('yeoman-generator')));
+}(require('yosay'), require('yeoman-generator')));
---
+++ post/templates/post.md
@@ -0,0 +1,6 @@
+---
+layout: <%= layout %>
+title: <%= name %>
+slug: <%= _.slugify(name) %>
+date: <%= date %> <%= time %>
+---
