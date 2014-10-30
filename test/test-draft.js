9371d52995b715fd947861775d6e55c7c1ed01d5
HiroAgustin <hiroagustin@gmail.com>
2014-10-30 15:17:10 -0400

    add unit tests for draft subgenerator

--- test/test-draft.js
+++ test/test-draft.js
@@ -1,26 +1,94 @@
-;(function (path, yeomanGenerator, os)
+;(function (yeoman, os, fs, path)
 {
   'use strict';
 
-  var assert = yeomanGenerator.assert
-    , helpers = yeomanGenerator.test;
+  var assert = yeoman.assert
+    , helpers = yeoman.test;
 
-  describe('jekyll-utils:post', function ()
+  function run ()
   {
-    before(function (done)
+    return helpers.run(path.join(__dirname, '../draft'))
+      .inDir(path.join(os.tmpdir(), './temp-test'), function (dir)
+      {
+        fs.mkdirSync(path.join(dir, './_layouts'));
+        fs.mkdirSync(path.join(dir, './_drafts'));
+      })
+      .withOptions({
+        'skip-greeting': true
+      });
+  }
+
+  function readFile (route)
+  {
+    return fs.readFileSync(route, {
+      encoding: 'utf-8'
+    }).trim().split('\n');
+  }
+
+  describe('jekyll-utils:draft', function ()
+  {
+    var date = new Date()
+      , prefix = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+
+    describe('default usage', function ()
     {
-      helpers.run(path.join(__dirname, '../draft'))
-        .inDir(path.join(os.tmpdir(), './temp-test'))
-        .withArguments('name', '--force')
-        .on('end', done);
+      before(function (done)
+      {
+        run()
+          .withPrompt({
+            name: 'My Awesome Draft'
+          , layout: 'post'
+          , extention: 'md'
+          })
+          .on('end', done);
+      });
+
+      it('creates the draft', function ()
+      {
+        assert.file('./' + prefix + '-my-awesome-draft.md');
+      });
+
+      it('with the selected content', function ()
+      {
+        var content = readFile('./' + prefix + '-my-awesome-draft.md');
+
+        assert.equal(content[0], '---');
+        assert.equal(content[1], 'layout: post');
+        assert.equal(content[2], 'title: My Awesome Draft');
+        assert.equal(content[3].indexOf('date: ' + prefix), 0);
+        assert.equal(content[4], '---');
+      });
     });
 
-    it('creates files', function ()
+    describe('with title in the arguments', function ()
     {
-      assert.file([
-        'somefile.js'
-      ]);
+      before(function (done)
+      {
+        run()
+          .withArguments('My Sweet Draft')
+          .withPrompt({
+            layout: 'default'
+          , extention: 'markdown'
+          })
+          .on('end', done);
+      });
+
+      it('creates the draft', function ()
+      {
+        assert.file('./' + prefix + '-my-sweet-draft.markdown');
+      });
+
+      it('with the selected content', function ()
+      {
+        var content = readFile('./' + prefix + '-my-sweet-draft.markdown');
+
+        assert.equal(content[0], '---');
+        assert.equal(content[1], 'layout: default');
+        assert.equal(content[2], 'title: My Sweet Draft');
+        assert.equal(content[3].indexOf('date: ' + prefix), 0);
+        assert.equal(content[4], '---');
+      });
     });
   });
 
-}(require('path'), require('yeoman-generator'), require('os')));
+}(require('yeoman-generator'), require('os'), require('fs'), require('path')));
