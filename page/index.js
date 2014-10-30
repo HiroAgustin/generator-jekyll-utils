768956f6ef3ca8ac4a476a7e803aa0dd6d063cbc
HiroAgustin <hiroagustin@gmail.com>
2014-10-30 11:31:59 -0400

    add support for creating pages

---
+++ page/index.js
@@ -0,0 +1,121 @@
+;(function (yosay, yeoman, fs, path)
+{
+  'use strict';
+
+  module.exports = yeoman.generators.Base.extend({
+
+    initializing: function ()
+    {
+      this.root = this.getRootPath();
+
+      this.name = Array.prototype.join.call(arguments, ' ');
+    }
+
+  , getRootPath: function ()
+    {
+      var cwd = process.cwd()
+        , oneUp = path.join(cwd, '..')
+        , onApp = path.join(cwd, 'app');
+
+      if (fs.existsSync(path.join(cwd, '_layouts')))
+        return cwd;
+
+      if (fs.existsSync(path.join(oneUp, '_layouts')))
+        return oneUp;
+
+      if (fs.existsSync(path.join(onApp, '_layouts')))
+        return onApp;
+    }
+
+  , prompting: function ()
+    {
+      var name = this.name
+        , root = this.root
+        , done = this.async();
+
+      // Have Yeoman greet the user.
+      this.log(yosay('Creating a page? I can help with that!'));
+
+      this.prompt(
+        [
+          {
+            type: 'input'
+          , name: 'name'
+          , message: 'What is the page title?'
+          , when: function ()
+            {
+              return !name;
+            }
+          }
+        , {
+            type: 'confirm'
+          , name: 'isFolder'
+          , message: 'Should it be inside a folder?'
+          , default: true
+          }
+        , {
+            type: 'list'
+          , name: 'layout'
+          , message: 'Which layout would you like to use?'
+          , default: 'page'
+          , choices: function ()
+            {
+              return fs.readdirSync(path.join(root, '_layouts')).map(function (name)
+              {
+                return name.split('.')[0];
+              });
+            }
+          }
+        , {
+            type: 'list'
+          , name: 'extention'
+          , message: 'Which file extention?'
+          , default: 'html'
+          , choices: [
+              'md'
+            , 'mkd'
+            , 'mkdn'
+            , 'html'
+            , 'mkdown'
+            , 'textile'
+            , 'markdown'
+            ]
+          , when: function (props)
+            {
+              return !props.isFolder;
+            }
+          }
+        ]
+
+      , function (props)
+        {
+          this.extention = props.extention;
+          this.name = name || props.name;
+          this.layout = props.layout;
+
+          this.isFolder = props.isFolder;
+
+          done();
+
+        }.bind(this)
+      );
+    }
+
+  , writing: function ()
+    {
+      if (this.isFolder)
+      {
+        this.destinationRoot(path.join(this.root, this._.slugify(this.name)));
+
+        this.template('page.html', 'index.html');
+      }
+      else
+      {
+        this.destinationRoot(this.root);
+
+        this.template('page.html', this._.slugify(this.name) + '.' + this.extention);
+      }
+    }
+  });
+
+}(require('yosay'), require('yeoman-generator'), require('fs'), require('path')));
